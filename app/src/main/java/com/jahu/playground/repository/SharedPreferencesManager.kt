afcc74664ce97a8984b7ae867fdc211f39451144
Jan Hudzik <jan.hudzik@gmail.com>
2017-10-07 23:42:19 +0200

    Using package level functions (instead of companion objects) to define some constants

--- app/src/main/java/com/jahu/playground/repository/SharedPreferencesManager.kt
+++ app/src/main/java/com/jahu/playground/repository/SharedPreferencesManager.kt
@@ -3,20 +3,14 @@ package com.jahu.playground.repository
 import android.content.Context
 import android.content.SharedPreferences
 
+private const val PREFS_NAME = "playground-prefs"
+private const val ACTUAL_USER_KEY = "actualUser"
+
 class SharedPreferencesManager(
         context: Context
 ) {
 
-    companion object {
-        private val PREFS_NAME = "playground-prefs"
-        private val ACTUAL_USER_KEY = "actualUser"
-    }
-
-    private val sharedPreferences: SharedPreferences
-
-    init {
-        sharedPreferences = context.getSharedPreferences(PREFS_NAME, 0)
-    }
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences(PREFS_NAME, 0)
 
     fun setActualUserNick(nick: String) {
         sharedPreferences.edit().putString(ACTUAL_USER_KEY, nick).apply()
