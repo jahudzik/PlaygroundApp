85480900bc857023627a91a305e5a14a3dc1bf12
Jan Hudzik <jan.hudzik@gmail.com>
2017-10-08 23:52:01 +0200

    Setup LeakCanary to watch all the activities
    https://github.com/square/leakcanary/wiki/FAQ

--- app/src/main/java/com/jahu/playground/PlaygroundApplication.kt
+++ app/src/main/java/com/jahu/playground/PlaygroundApplication.kt
@@ -2,11 +2,18 @@ package com.jahu.playground
 
 import android.app.Application
 import android.os.StrictMode
+import com.squareup.leakcanary.LeakCanary
 
 class PlaygroundApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
         setupStrictMode()
     }
 
