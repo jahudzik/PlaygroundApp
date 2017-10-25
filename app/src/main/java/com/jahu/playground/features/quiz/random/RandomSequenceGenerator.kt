614cccd7477f70bdd2518091c0ae1efe6f100935
Jan Hudzik <jan.hudzik@gmail.com>
2017-10-25 23:37:54 +0200

    Fix in RandomSequenceGenerator

--- app/src/main/java/com/jahu/playground/features/quiz/random/RandomSequenceGenerator.kt
+++ app/src/main/java/com/jahu/playground/features/quiz/random/RandomSequenceGenerator.kt
@@ -6,8 +6,8 @@ class RandomSequenceGenerator(
 
     fun generate(size: Int): List<Int> {
         val baseList = (0 until size).toMutableList()
-        return (0 until size)
-                .map { numberGenerator.nextNumber(it) }
+        return (size - 1 downTo 0)
+                .map { numberGenerator.nextNumber(it + 1) }
                 .map { baseList.removeAt(it) }
     }
 
